1df14941101c23d0157947eb7fb01a867a2b47d7
Joachim256 <jachymnet@seznam.cz>
2023-08-10 09:27:54 +0200

    Pokusit se opravit skript

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-git pull --all
-git checkout website
+git switch website
+git pull
 git config --global user.name "Joachim256"
 git config --global user.email "Joachim256@users.noreply.github.com"
 
@@ -9,11 +9,11 @@ git config --global user.email "Joachim256@users.noreply.github.com"
 curl -sS -c ~/cookies -H "x-ig-app-id: 936619743392459" -H "Cookie: sessionid=$IGSESSIONID" https://www.instagram.com/api/v1/users/10005941662/info/
 printf ".instagram.com\tTRUE\t/\tTRUE\t2521843200\tsessionid\t%s\n" "$IGSESSIONID" >> ~/cookies
 # request to get data
-bio=$(curl -b ~/cookies -H "x-ig-app-id: 936619743392459" https://www.instagram.com/api/v1/users/10005941662/info/ | jq '.user.biography')
+bio=$(curl -sS -b ~/cookies -H "x-ig-app-id: 936619743392459" https://www.instagram.com/api/v1/users/10005941662/info/ | jq '.user.biography')
 
 echo "Bio is $bio"
 
-if [[ "$bio" == "" ]]; then
+if [ "$bio" == "" ]; then
 	echo "Failed to fetch bio"
 	exit 1
 
