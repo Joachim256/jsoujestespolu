fedb15391d167868a40bae109789512524927692
Joachim256 <jachymnet@seznam.cz>
2023-08-10 09:18:21 +0200

    Zkusit opravit akci

--- update.sh
+++ update.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
+git pull --all
 git checkout website
 git config --global user.name "Joachim256"
 git config --global user.email "Joachim256@users.noreply.github.com"
 
 # request to get cookies
-curl -c ~/cookies -H "x-ig-app-id: 936619743392459" -H "Cookie: sessionid=$IGSESSIONID" https://www.instagram.com/api/v1/users/10005941662/info/ 2> /dev/null
+curl -sS -c ~/cookies -H "x-ig-app-id: 936619743392459" -H "Cookie: sessionid=$IGSESSIONID" https://www.instagram.com/api/v1/users/10005941662/info/
 printf ".instagram.com\tTRUE\t/\tTRUE\t2521843200\tsessionid\t%s\n" "$IGSESSIONID" >> ~/cookies
 # request to get data
 bio=$(curl -b ~/cookies -H "x-ig-app-id: 936619743392459" https://www.instagram.com/api/v1/users/10005941662/info/ | jq '.user.biography')
 
-if [[ $bio == *"17.11.2022🤍"* ]] || [[ $bio == *"@eli._fili"* ]]; then
+echo "Bio is $bio"
+
+if [[ "$bio" == "" ]]; then
+	echo "Failed to fetch bio"
+	exit 1
+
+if [[ "$bio" == *"17.11.2022🤍"* ]] || [[ "$bio" == *"@eli._fili"* ]]; then
 	if [ $(cat prev) == "0" ]; then
 		echo "1" > prev
 		git checkout main -- jestejsouspolu.html
